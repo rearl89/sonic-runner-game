afe0c4068726c8f9bb088b24e2c867def5c8c0b4
Roger Earl <rogerearl@me.com>
2024-10-27 23:28:44 -0700

    Added rings to game and scoring.

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,6 +1,7 @@
 import k from "../kaplayCtx";
 import { makeSonic } from "../entities/sonic";
 import { makeMotobug } from "../entities/motobug";
+import { makeRing } from "../entities/ring";
 
 export default function game() {
   k.setGravity(3100);
@@ -21,9 +22,41 @@ export default function game() {
     k.add([k.sprite("platforms"), k.pos(384 * 4, 450), k.scale(4)]),
   ];
 
+  let score = 0;
+  let scoreMultiplier = 0;
+
+  const scoreText = k.add([
+    k.text("SCORE : 0", { font: "mania", size: 72 }),
+    k.pos(20, 20),
+  ]);
+
   const sonic = makeSonic(k.vec2(200, 745));
   sonic.setControls();
   sonic.setEvents();
+  sonic.onCollide("enemy", (enemy) => {
+    if (!sonic.isGrounded()) {
+      k.play("destroy", { volume: 0.5 });
+      k.play("hyper-ring", { volume: 0.5 });
+      k.destroy(enemy);
+      sonic.play("jump");
+      sonic.jump();
+      scoreMultiplier += 1;
+      score += 10 * scoreMultiplier;
+      scoreText.text = `SCORE : ${score}`;
+      return;
+    }
+
+    k.play("hurt", { volume: 0.5 });
+
+    k.go("gameover");
+  });
+
+  sonic.onCollide("ring", (ring) => {
+    k.play("ring", { volume: 0.5 });
+    k.destroy(ring);
+    score++;
+    scoreText.text = `SCORE : ${score}`;
+  });
 
   let gameSpeed = 300;
   k.loop(1, () => {
@@ -49,6 +82,21 @@ export default function game() {
     k.wait(waitTime, spawnMotobug);
   };
   spawnMotobug();
+
+  const spawnRing = () => {
+    const ring = makeRing(k.vec2(1950, 745));
+    ring.onUpdate(() => {
+      ring.move(-gameSpeed, 0);
+    });
+    ring.onExitScreen(() => {
+      if (ring.pos.x < 0) k.destroy(ring);
+    });
+
+    const waitTime = k.rand(0.5, 3);
+
+    k.wait(waitTime, spawnRing);
+  };
+  spawnRing();
 
   k.add([
     k.rect(1920, 3000),
